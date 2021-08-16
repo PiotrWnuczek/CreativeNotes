ad7855fd01c4f0f13c93916f12c8907799d52c38
Piotr Wnuczek <piotrantoniwnuczek@gmail.com>
2021-08-16 14:26:33 +0200

    firebase

--- src/views/Root.js
+++ src/views/Root.js
@@ -6,6 +6,23 @@ import { store, persistor } from 'logic/store';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import { Route, Redirect } from 'react-router-dom';
 import Main from 'views/Main';
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbyMzDj4bnxURJI6PCHVKV-hz6PG2qU-I",
+  authDomain: "creativenotebook-450e5.firebaseapp.com",
+  projectId: "creativenotebook-450e5",
+  storageBucket: "creativenotebook-450e5.appspot.com",
+  messagingSenderId: "664350679572",
+  appId: "1:664350679572:web:f8245b38b6e857fdbc3d01",
+  measurementId: "G-XZJ57FCFSB"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 <link href='https://fonts.googleapis.com/css2?family=Lato:wght@400;700' />
 
@@ -23,7 +40,7 @@ const Root = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
-      <GlobalStyle />
+        <GlobalStyle />
         <Switch>
           <Route exact path='/' render={() => <Redirect to='/start' />} />
           <Route exact path='/start'><Main /></Route>
